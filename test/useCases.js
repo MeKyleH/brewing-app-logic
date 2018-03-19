64743102310b3f05d4963848ab9005f0199f32d9
severnsc <severnsc@gmail.com>
2018-03-19 17:09:13 -0400

    Add all initial basic usecases

--- test/useCases.js
+++ test/useCases.js
@@ -1,12 +1,12 @@
 const core = require('../lib')
 const chai = require('chai')
 const should = chai.should()
-const expect = chai.expect
 
 describe('create timer use case', () => {
   describe('happy path', () => {
     it('should return timer entity', () => {
-      const timer = core.createTimerUseCase(1000, 1000)
+      const createTimer = () => console.log("timer created!")
+      const timer = core.createTimerUseCase(createTimer)(1000,1000)
       timer.should.be.an('object')
       timer.should.have.property('id')
       timer.id.should.be.a('string')
@@ -28,6 +28,7 @@ describe('create timer use case', () => {
 
 describe('start timer use case', () => {
   describe('happy path', () => {
+
     it('should return the timer with isRunning true', () => {
       const getTimerById = () =>  ({
         id: "1",
@@ -38,7 +39,7 @@ describe('start timer use case', () => {
       })
       const saveTimer = () => console.log('timer saved!')
       const startedTimer = core.startTimerUseCase(getTimerById)(saveTimer)("1")
-      startedTimer.should.be.a('object')
+      startedTimer.should.be.an('object')
       startedTimer.id.should.equal("1")
       startedTimer.durationInMs.should.equal(1000)
       startedTimer.remainingDurationInMs.should.equal(1000)
@@ -51,7 +52,43 @@ describe('start timer use case', () => {
 describe('stop timer use case', () => {
   describe('happy path', () => {
     it('should return the timer with isRunning false', () => {
+      const getTimerById = () =>  ({
+        id: "1",
+        durationInMs: 1000,
+        remainingDurationInMs: 1000,
+        intervalDurationInMs: 1000,
+        isRunning: true
+      })
+      const saveTimer = () => console.log('timer saved!')
+      const stoppedTimer = core.stopTimerUseCase(getTimerById)(saveTimer)("1")
+      stoppedTimer.should.be.an('object')
+      stoppedTimer.id.should.equal("1")
+      stoppedTimer.durationInMs.should.equal(1000)
+      stoppedTimer.remainingDurationInMs.should.equal(1000)
+      stoppedTimer.intervalDurationInMs.should.equal(1000)
+      stoppedTimer.isRunning.should.equal(false)
+    })
+  })
+})
 
+describe('decrement timer use case', () => {
+  describe('happy path', () => {
+    it('should return timer decremented by interval', () => {
+      const getTimerById = () =>  ({
+        id: "1",
+        durationInMs: 1000,
+        remainingDurationInMs: 1000,
+        intervalDurationInMs: 500,
+        isRunning: true
+      })
+      const saveTimer = () => console.log('timer saved!')
+      const decrementedTimer = core.decrementTimerUseCase(getTimerById)(saveTimer)("1")
+      decrementedTimer.should.be.an('object')
+      decrementedTimer.id.should.equal("1")
+      decrementedTimer.durationInMs.should.equal(1000)
+      decrementedTimer.remainingDurationInMs.should.equal(500)
+      decrementedTimer.intervalDurationInMs.should.equal(500)
+      decrementedTimer.isRunning.should.equal(true)
     })
   })
 })
@@ -59,7 +96,21 @@ describe('stop timer use case', () => {
 describe('reset timer use case', () => {
   describe('happy path', () => {
     it('should return the timer with remainingDuration === duration', () => {
-
+      const getTimerById = () =>  ({
+        id: "1",
+        durationInMs: 1000,
+        remainingDurationInMs: 200,
+        intervalDurationInMs: 1000,
+        isRunning: true
+      })
+      const saveTimer = () => console.log('timer saved!')
+      const resetTimer = core.resetTimerUseCase(getTimerById)(saveTimer)("1")
+      resetTimer.should.be.an('object')
+      resetTimer.id.should.equal("1")
+      resetTimer.durationInMs.should.equal(1000)
+      resetTimer.remainingDurationInMs.should.equal(1000)
+      resetTimer.intervalDurationInMs.should.equal(1000)
+      resetTimer.isRunning.should.equal(true)
     })
   })
 })
