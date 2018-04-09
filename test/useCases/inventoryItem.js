e257c0e00860a66966b0251aeaef289bcafea2fa
severnsc <severnsc@gmail.com>
2018-04-09 13:27:46 -0400

    Update updatedAt date when updateInventoryItemUseCase is called

--- test/useCases/inventoryItem.js
+++ test/useCases/inventoryItem.js
@@ -288,7 +288,12 @@ describe.only('inventory item use cases', () => {
       })
 
       it('should return an inventoryItem whose props are updated to match updatePropsObj', () => {
-        updatedInventoryItem.should.deep.equal(Object.assign({}, inventoryItem, updatePropsObj))
+        const updatedInventoryItemNoDate = Object.assign({}, updatedInventoryItem, {updatedAt: null})
+        updatedInventoryItemNoDate.should.deep.equal(Object.assign({}, inventoryItem, Object.assign({}, updatePropsObj, {updatedAt: null})))
+      })
+
+      it('should update updatedAt date', () => {
+        updatedInventoryItem.updatedAt.should.not.equal(inventoryItem.updatedAt)
       })
 
     })
@@ -380,7 +385,8 @@ describe.only('inventory item use cases', () => {
       it('should not throw an error', () => {
         const nullUpdateObj = {lastReorderDate: null, deliveryDate: null}
         const updatePropsObj2 = curriedUpdateInventoryItemUseCase(id, nullUpdateObj)
-        updatePropsObj2.should.deep.equal(Object.assign({}, inventoryItem, nullUpdateObj))
+        should.equal(null, updatePropsObj2.lastReorderDate)
+        should.equal(null, updatePropsObj2.deliveryDate)
       })
 
     })
